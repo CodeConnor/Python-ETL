b1d863a026d201f0df41e1e3f26e34ce0108ae83
Connor <xiaobang199806@163.com>
2023-05-18 19:03:34 +0800

    update file_name

--- config/project_config.py
+++ config/project_config.py
@@ -1,6 +1,8 @@
 '''
 整个项目的配置项都设置在这个文件中
 '''
+import time
+
 
 # ============================= mysql 配置 ==================================
 metadata_host = 'localhost'
@@ -11,7 +13,8 @@ metadata_password = '123456'
 
 # =========================== 程序运行日志配置项 ================================
 # 日志输出根目录
-
-# 日志输出文件名
-
+file_path = 'D:\Python\PycharmProjects\Python-ETL\log'
+# 日志输出文件名(每小时生成1个log)
+log_time = time.strftime("%Y%m%d-%H", time.localtime(time.time()))
+file_name = f'pyetl-{log_time}.log'
 # ============================================================================
