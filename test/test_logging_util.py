14decfb53c70545fdc0f844d67a599558b557f8b
Connor <xiaobang199806@163.com>
2023-05-19 11:09:08 +0800

    update test

--- test/test_logging_util.py
+++ test/test_logging_util.py
@@ -1,13 +1,17 @@
 # coding:utf8
 # 导入单元测试包和需要测试的文件
+import logging
 from unittest import TestCase
 from util.logging_util import Logging
 
+
 # 必须定义的测试类
 class TestLoggingUtil(TestCase):
+    # 初始化需要测试的类
     def setUp(self) -> None:
         self.logging_util = Logging()
 
-    def test_logging(self):
-        self.logging_util.init_logger()
-
+    # 定义测试方法
+    def test_init_logging(self):
+        logger = self.logging_util.init_logger()
+        self.assertIsInstance(logger, logging.RootLogger)
