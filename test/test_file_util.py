2f3b5e65c6c7c3855f1d1f7c07e47a7abb8f0439
Connor <xiaobang199806@163.com>
2023-05-19 14:20:13 +0800

    update test_fileââ_util

--- test/test_file_util.py
+++ test/test_file_util.py
@@ -12,4 +12,18 @@ class TestFileUtil(TestCase):
         pass
 
     def get_dir_files_list(self):
+        '''
+        请在工程根目录的test文件夹内建立:
+        test_dir/
+            inner1/
+                3
+                4
+                inner2/
+                    5
+        1
+        2
+        的目录结构用于进行此方法的单元测试
+        不递归结果应该是1和2
+        递归结果应该是1,2,3,4,5
+        '''
         pass
