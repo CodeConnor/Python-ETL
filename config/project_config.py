14807e811a28fe56459cd32f6529724cc0625f45
Connor <xiaobang199806@163.com>
2023-05-19 18:31:16 +0800

    update mysql_util

--- config/project_config.py
+++ config/project_config.py
@@ -4,14 +4,6 @@
 import time
 
 
-# ============================= mysql 配置 start ==================================
-metadata_host = 'localhost'
-metadata_port = 3306
-metadata_user = 'root'
-metadata_password = '123456'
-# ============================= mysql 配置 end ====================================
-
-
 # =========================== 程序运行日志配置项 start ================================
 # 日志输出根目录
 file_path = 'D:\Python\PycharmProjects\Python-ETL\log\\'
@@ -26,3 +18,14 @@ file_name = f'pyetl-{log_time}.log'
 
 #
 # =========================== JSON订单数据采集配置项 end ==================================
+
+
+# ============================= mysql 配置 start ==================================
+metadata_host = 'localhost'
+metadata_port = 3306
+metadata_user = 'root'
+metadata_password = '123456'
+mysql_charset = 'utf8'
+# ============================= mysql 配置 end ====================================
+
+
--- util/mysql_util.py
+++ util/mysql_util.py
@@ -7,25 +7,22 @@ from config import project_config as conf
 # 创建pymysql工具类
 class MySQLUtil(object):
     # 初始化MySQL配置信息为属性
-    def __init__(self,
-                 host=conf.metadata_host,
-                 port=conf.metadata_port,
-                 user=conf.metadata_user,
-                 password=conf.metadata_password
-                 ):
+    def __init__(self):
         # 创建pymysql对象，连接数据库
-        self.conn = pymysql.connect(
-            host=host,
-            port=port,
-            user=user,
-            password=password
+        self.conn = pymysql.Connection(
+            host=conf.metadata_host,            # 主机名，IP地址
+            port=conf.metadata_port,            # 端口号
+            user=conf.metadata_user,            # 用户
+            password=conf.metadata_password,    # 密码
+            charset=conf.mysql_charset,         # 设置字符集
+            autocommit=False                    # sql语句的自动提交
         )
 
     # 封装方法执行SQL语句
     def execute(self, sql):
         # 获取可执行sql的游标cursor
         cursor = self.conn.cursor()
-        # 执行传入游标中的SQL语句
+        # 执行传入游标中的SQL语句，此时代码还不会提交到数据库中执行，需要执行`self.conn.commit()`后才会执行（autocommit=False）
         cursor.execute(sql)
         # 关闭游标
         cursor.close()
