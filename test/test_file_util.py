9bbfd3f195220e59548506062e9a852bb7eb80fa
Connor <xiaobang199806@163.com>
2023-05-19 14:10:45 +0800

    update file_util and create test_file_util

---
+++ test/test_file_util.py
@@ -0,0 +1,15 @@
+# coding:utf8
+'''
+单元测试：
+测试file_util中的方法
+'''
+from unittest import TestCase
+from util.file_util import get_dir_files_list
+
+
+class TestFileUtil(TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def get_dir_files_list(self):
+        pass
--- util/file_util.py
+++ util/file_util.py
@@ -3,3 +3,14 @@
 功能逻辑：
 封装文件处理的相关方法，实现文件处理在其他代码中的复用
 '''
+import os
+
+
+def get_dir_files_list(path='./', recursion=False):
+    '''
+    获取文件夹中的文件列表，可进行递归获取
+    :param path: 需要进行文件判断的文件路径，默认当前目录
+    :param recursion: 是否递归读取，默认不递归
+    :return: list对象，其中储存文件路径
+    '''
+    pass
