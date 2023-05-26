ccf5f628b0164ca2f0e59ad76464435fae4e01dc
Connor <xiaobang199806@163.com>
2023-05-26 17:45:18 +0800

    update json_service

--- json_service.py
+++ json_service.py
@@ -21,3 +21,7 @@ logger.info(f'读取元数据库，所获已处理文件如下：{processed_file
 # 通过对比JSON文件列表与已处理文件列表，找出待处理文件
 files_to_be_processed = fu.get_new_files_by_comparing_lists(files, processed_files)
 logger.info(f'通过对比元数据库，待处理文件如下：{files_to_be_processed}')
+
+# 对待处理文件进行读取，按行读取，防止一次性读取文件中所有信息导致性能下降
+
+# 通过模型(将数据封装到类里面,一个字段,作为类的一个成员变量)来进行数据处理
