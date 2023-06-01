80493b7ac74f4fda7aa54a052c7933f96df0d632
Connor <xiaobang199806@163.com>
2023-06-01 17:51:15 +0800

    feat: query source_data

--- mysql_service.py
+++ mysql_service.py
@@ -33,3 +33,45 @@ if not source_db_util.check_table_exists(conf.source_db_name, conf.source_barcod
     logger.error(f'数据源库：{conf.source_db_name}中不存在数据源表：{conf.source_barcode_table_name}，'
                  f'无法采集，程序终止，请核实原因...')
     sys.exit(1)  # 发生异常，终止程序
+
+# 判断目标数据库中是否存在保存数据源的表，没有就创建
+target_db_util.check_table_exists_and_create(
+    conf.target_db_name,
+    conf.target_barcode_table_name,
+    conf.target_barcode_table_create_cols
+)
+
+last_update_time = None  # 记录元数据表的上次更新时间，初始为空
+# 判断元数据库是否存在barcode元数据表
+metadata_db_util.select_db(conf.metadata_db_name)
+if not metadata_db_util.check_table_exists(conf.metadata_db_name, conf.metadata_barcode_monitor_table_name):
+    # 不存在该表则创建
+    metadata_db_util.check_table_exists_and_create(
+        conf.metadata_db_name,
+        conf.metadata_barcode_monitor_table_name,
+        conf.metadata_barcode_monitor_table_create_cols
+    )
+else:
+    # 筛选出上次的元数据更新时间，即最大的time_record
+    # 但是不能用MAX()，因为使用MAX()如果查不到数据会返回((None),)，而不是真正的空()
+    query_sql = f"SELECT time_record FROM {conf.metadata_barcode_monitor_table_name} " \
+                f"ORDER BY time_record DESC LIMIT 1"
+    result = metadata_db_util.query(query_sql)
+
+    if len(result) != 0:
+        # 如果能查到最大time_record，就取出元组中的字符 ==> ((timestamp, ), )
+        last_update_time = str(result[0][0])
+
+# 判断元数据上次更新时间是否存在
+if last_update_time:
+    # 存在则用该时间筛选数据源表
+    barcode_query_sql = f"SELECT * FROM {conf.source_barcode_table_name} " \
+          f"WHERE updateAt >= '{last_update_time}' ORDER BY updateAt"
+else:
+    # 不存在则检索整个数据源表
+    barcode_query_sql = f"SELECT * FROM {conf.source_barcode_table_name} " \
+                        f"ORDER BY updateAt"
+
+# 执行查询
+source_db_util.select_db(conf.source_db_name)
+result = source_db_util.query(barcode_query_sql)
