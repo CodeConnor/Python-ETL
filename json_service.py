79095d5550c33999279189aaa23252b95b01a206
Connor <xiaobang199806@163.com>
2023-06-01 11:10:56 +0800

    update metadata_insert

--- json_service.py
+++ json_service.py
@@ -8,7 +8,7 @@ from model.retail_orders_model import OrdersModel, OrdersDetailModel
 
 # TODO: 步骤1--读取文件，获取待处理文件
 # 构建数据库连接
-orders_db_util = MySQLUtil()  # 建立元数据库连接
+metadata_db_util = MySQLUtil()  # 建立元数据库连接
 # 建立目标数据库连接
 target_db_util = MySQLUtil(
     host=conf.target_host,
@@ -27,7 +27,7 @@ files = fu.get_dir_files_list(conf.json_root_path)
 logger.info(f'读取JSON数据路径，所获文件如下：{files}')
 
 # 将已处理JSON数据文件信息存入元数据库
-processed_files = get_processed_files(orders_db_util)
+processed_files = get_processed_files(metadata_db_util)
 logger.info(f'读取元数据库，所获已处理文件如下：{processed_files}')
 
 # 通过对比JSON文件列表与已处理文件列表，找出待处理文件
@@ -37,10 +37,12 @@ logger.info(f'通过对比元数据库，待处理文件如下：{files_to_be_pr
 # TODO: 步骤二--开始处理数据
 global_count = 0  # 记录被处理数据的行数，全局记录
 global_count_reserved = 0  # 记录过滤后保留的数据行数，全局记录
+dict_processed_files = {}  # 记录被处理的文件与文件中被处理的数据条数
 # 对待处理文件进行读取，按行读取，防止一次性读取文件中所有信息导致性能下降
 for file in files_to_be_processed:
     order_model_list = []  # 存储所有订单模型对象
     order_detail_model_list = []  # 存储所有订单详情模型对象
+    count_processed_lines = 0  # 记录单个文件被处理的数据行数
 
     for line in open(file, 'r', encoding='UTF-8'):
         line = line.replace('\n', '')  # line是文件中的1行数据，需要将换行符替换为空字符
@@ -54,14 +56,15 @@ for file in files_to_be_processed:
             order_detail_model = OrdersDetailModel(line)
             order_detail_model_list.append(order_detail_model)
             global_count_reserved += 1
+            count_processed_lines += 1
 
     # 将订单模型的中的数据写出到CSV文件
     order_csv_write_f = open(  # 用于写出订单模型的文件对象，使用追加模式，防止写入多个文件内容时，将上一次写入内容覆盖
-        conf.retail_output_csv_root_path + conf.retail_orders_output_csv_file_name, 'w', encoding='UTF-8'
+        conf.retail_output_csv_root_path + conf.retail_orders_output_csv_file_name, 'a', encoding='UTF-8'
     )
 
     order_detail_csv_write_f = open(  # 用于写出订单详情模型的文件对象
-        conf.retail_output_csv_root_path + conf.retail_orders_detail_output_csv_file_name, 'w', encoding='UTF-8'
+        conf.retail_output_csv_root_path + conf.retail_orders_detail_output_csv_file_name, 'a', encoding='UTF-8'
     )
 
     for model in order_model_list:
@@ -111,9 +114,25 @@ for file in files_to_be_processed:
         target_db_util.select_db(conf.target_db_name)
         target_db_util.execute_without_autocommit(orders_detail_sql)
 
+    dict_processed_files[file] = count_processed_lines  # 将被处理的文件名与数据行数记录到字典中
+
 target_db_util.conn.commit()  # 一次性提交所有SQL
 logger.info(f'CSV备份文件写出完成，写出路径：{conf.retail_output_csv_root_path}')
 logger.info(f'json数据写入target数据库成功，共处理：{global_count}行，写入：{global_count_reserved}行，过滤：{global_count - global_count_reserved}行')
+
+# 将已处理文件的记录存入元数据库中
+for file_name in dict_processed_files.keys():  # 取出文件名
+    lines_processed_file = dict_processed_files[file_name]  # 取出对应文件被处理行数
+    insert_sql = f"INSERT IGNORE INTO {conf.metadata_file_monitor_table_name}(file_name, process_lines) " \
+          f"VALUES ('{file_name}', {lines_processed_file})"  # 组成插入语句，只用插入两列
+    metadata_db_util.execute(insert_sql)
+
+# 关闭所有数据库连接
+metadata_db_util.close_conn()
+target_db_util.close_conn()
+logger.info(f'读取JSON数据写入数据库完成，CSV文件备份完成，程序结束......')
+
+
 
 
 
