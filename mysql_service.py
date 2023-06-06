32b59e223fbd916c1c1b31161511778b10e4ae7d
Connor <xiaobang199806@163.com>
2023-06-06 10:38:28 +0800

    feat: create barcode_models

--- mysql_service.py
+++ mysql_service.py
@@ -6,6 +6,7 @@ import sys
 from util.logging_util import Logging
 from util.mysql_util import MySQLUtil
 from config import project_config as conf
+from model.barcode_model import BarcodeModel
 
 # 构建日志对象
 logger = Logging().init_logger()
@@ -75,6 +76,45 @@ else:
 # 执行查询
 source_db_util.select_db(conf.source_db_name)
 result = source_db_util.query(barcode_query_sql)
+# pymysql的查询结果为((col1, col2, col3), (col1, col2, col3), (col1, col2, col3), )
+# 每个小元组是整个结果的1行，每个小元组内的元素是结果中每个列的1个元素
+# TODO: 步骤三 -- 构建模型
+barcode_models = []
+for single_line in result:
+    # 每行结果中列的排列顺序与表格顺序相同（select *）
+    code = single_line[0]
+    name = single_line[1]
+    spec = single_line[2]
+    trademark = single_line[3]
+    addr = single_line[4]
+    units = single_line[5]
+    factory_name = single_line[6]
+    trade_price = single_line[7]
+    retail_price = single_line[8]
+    update_at = single_line[9]
+    wholeunit = single_line[10]
+    wholenum = single_line[11]
+    img = single_line[12]
+    src = single_line[13]
 
-# TODO: 步骤三 -- 使用模型构建 INSERT 插入语句
+    model = BarcodeModel(
+        code=code,
+        name=name,
+        spec=spec,
+        trademark=trademark,
+        addr=addr,
+        units=units,
+        factory_name=factory_name,
+        trade_price=trade_price,
+        retail_price=retail_price,
+        update_at=update_at,
+        wholeunit=wholeunit,
+        wholenum=wholenum,
+        img=img,
+        src=src,
+    )
+    barcode_models.append(model)
+
+# TODO: 步骤四 -- 写入目标数据库
+
 
